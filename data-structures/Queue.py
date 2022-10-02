84c831b842b1fdfbe3b180dde3156b8228b1c5a3
Rodel Cabubas <rodel.cabubas@cebupacificair.com>
2022-10-02 09:21:38 +0800

    add queue

---
+++ data-structures/Queue.py
@@ -0,0 +1,31 @@
+from collections import deque
+from Stack import Stack
+
+# STACK = first in, first out (FIIFO)
+
+
+class Queue():
+    def __init__(self):
+        # use stack data structure
+        self.queue = Stack()
+        
+
+    def enqueue(self, data):
+        self.queue.push(data)
+
+    def dequeue(self):
+        self.queue.stack = self.queue.stack[1:] # first out always
+
+    def printData(self):
+        self.queue.printData()
+
+
+if __name__ == "__main__":
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    queue.dequeue()
+    queue.dequeue()
+    queue.printData()
